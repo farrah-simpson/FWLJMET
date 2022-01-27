4edc67c7b223987e1567851a76e8ca901737ff4f
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2022-01-27 11:31:53 -0500

    Update and rename runFWLJMET_singleLep2016_multipleTree_UL.py to runFWLJMet_singleLep2016_multipleTree_UL.py

--- LJMet/runFWLJMet_singleLep2016_multipleTree_UL.py
+++ LJMet/runFWLJMet_singleLep2016_multipleTree_UL.py
@@ -299,13 +299,13 @@ process.prefiringweight = l1ECALPrefiringWeightProducer.clone(
 
 from PhysicsTools.SelectorUtils.pfJetIDSelector_cfi import pfJetIDSelector
 pfJetIDSelector.version = cms.string('RUN2ULCHS')
-pfJetIDSelector.quality = cms.string('LOOSE')
+pfJetIDSelector.quality = cms.string('TIGHT') # this used to be LOOSE
 process.tightAK4Jets = cms.EDFilter("PFJetIDSelectionFunctorFilter",
                                     filterParams =pfJetIDSelector.clone(),
                                     src = cms.InputTag("updatedPatJets"),
 )
 
-pfJetIDSelector.version = cms.string( "RUN2ULPUPPI" )
+# pfJetIDSelector.version = cms.string( "RUN2ULPUPPI" ) # intending to re-use the same AK4 ID
 process.tightPackedJetsAK8Puppi = cms.EDFilter("PFJetIDSelectionFunctorFilter",
                                                filterParams =pfJetIDSelector.clone(),
                                                src = cms.InputTag("packedJetsAK8Puppi"),
@@ -327,24 +327,24 @@ JECdown                  = False
 JERup                    = False
 JERdown                  = False
 doAllJetSyst             = False #this determines whether to save JEC/JER up/down in one job. Default is currently false. Mar 19,2019.
-JEC_txtfile              = 'FWLJMET/LJMet/data/Summer16V11/Summer16_07Aug2017_V11_MC_Uncertainty_AK4PFchs.txt'
-JERSF_txtfile            = 'FWLJMET/LJMet/data/Summer16V1/Summer16_25nsV1_MC_SF_AK4PFchs.txt'
-JER_txtfile              = 'FWLJMET/LJMet/data/Summer16V1/Summer16_25nsV1_MC_PtResolution_AK4PFchs.txt'
-JERAK8_txtfile           = 'FWLJMET/LJMet/data/Summer16V1/Summer16_25nsV1_MC_PtResolution_AK8PFPuppi.txt'
-MCL1JetPar               = 'FWLJMET/LJMet/data/Summer16V11/Summer16_07Aug2017_V11_MC_L1FastJet_AK4PFchs.txt'
-MCL2JetPar               = 'FWLJMET/LJMet/data/Summer16V11/Summer16_07Aug2017_V11_MC_L2Relative_AK4PFchs.txt'
-MCL3JetPar               = 'FWLJMET/LJMet/data/Summer16V11/Summer16_07Aug2017_V11_MC_L3Absolute_AK4PFchs.txt'
-MCL1JetParAK8            = 'FWLJMET/LJMet/data/Summer16V11/Summer16_07Aug2017_V11_MC_L1FastJet_AK8PFPuppi.txt'
-MCL2JetParAK8            = 'FWLJMET/LJMet/data/Summer16V11/Summer16_07Aug2017_V11_MC_L2Relative_AK8PFPuppi.txt'
-MCL3JetParAK8            = 'FWLJMET/LJMet/data/Summer16V11/Summer16_07Aug2017_V11_MC_L3Absolute_AK8PFPuppi.txt'
-DataL1JetPar             = 'FWLJMET/LJMet/data/Summer16V11/Summer16_07Aug2017BCD_V11_DATA_L1FastJet_AK4PFchs.txt'
-DataL2JetPar             = 'FWLJMET/LJMet/data/Summer16V11/Summer16_07Aug2017BCD_V11_DATA_L2Relative_AK4PFchs.txt'
-DataL3JetPar             = 'FWLJMET/LJMet/data/Summer16V11/Summer16_07Aug2017BCD_V11_DATA_L3Absolute_AK4PFchs.txt'
-DataResJetPar            = 'FWLJMET/LJMet/data/Summer16V11/Summer16_07Aug2017BCD_V11_DATA_L2L3Residual_AK4PFchs.txt'
-DataL1JetParAK8          = 'FWLJMET/LJMet/data/Summer16V11/Summer16_07Aug2017BCD_V11_DATA_L1FastJet_AK8PFPuppi.txt'
-DataL2JetParAK8          = 'FWLJMET/LJMet/data/Summer16V11/Summer16_07Aug2017BCD_V11_DATA_L2Relative_AK8PFPuppi.txt'
-DataL3JetParAK8          = 'FWLJMET/LJMet/data/Summer16V11/Summer16_07Aug2017BCD_V11_DATA_L3Absolute_AK8PFPuppi.txt'
-DataResJetParAK8         = 'FWLJMET/LJMet/data/Summer16V11/Summer16_07Aug2017BCD_V11_DATA_L2L3Residual_AK8PFPuppi.txt'
+JEC_txtfile              = 'FWLJMET/LJMet/data/Summer19UL16_V7/Summer19UL16_V7_MC_Uncertainty_AK4PFchs.txt'
+JERSF_txtfile            = 'FWLJMET/LJMet/data/Summer20UL16_JRV3/Summer20UL16_JRV3_MC_SF_AK4PFchs.txt'
+JER_txtfile              = 'FWLJMET/LJMet/data/Summer20UL16_JRV3/Summer20UL16_JRV3_MC_PtResolution_AK4PFchs.txt'
+JERAK8_txtfile           = 'FWLJMET/LJMet/data/Summer20UL16_JRV3/Summer20UL16_JRV3_MC_PtResolution_AK8PFPuppi.txt'
+MCL1JetPar               = 'FWLJMET/LJMet/data/Summer19UL16_V7/Summer19UL16_V7_MC_L1FastJet_AK4PFchs.txt'
+MCL2JetPar               = 'FWLJMET/LJMet/data/Summer19UL16_V7/Summer19UL16_V7_MC_L2Relative_AK4PFchs.txt'
+MCL3JetPar               = 'FWLJMET/LJMet/data/Summer19UL16_V7/Summer19UL16_V7_MC_L3Absolute_AK4PFchs.txt'
+MCL1JetParAK8            = 'FWLJMET/LJMet/data/Summer19UL16_V7/Summer19UL16_V7_MC_L1FastJet_AK8PFPuppi.txt'
+MCL2JetParAK8            = 'FWLJMET/LJMet/data/Summer19UL16_V7/Summer19UL16_V7_MC_L2Relative_AK8PFPuppi.txt'
+MCL3JetParAK8            = 'FWLJMET/LJMet/data/Summer19UL16_V7/Summer19UL16_V7_MC_L3Absolute_AK8PFPuppi.txt'
+DataL1JetPar             = 'FWLJMET/LJMet/data/Summer19UL16_V7/Summer19UL16_RunFGH_V7_DATA_L1FastJet_AK4PFchs.txt'
+DataL2JetPar             = 'FWLJMET/LJMet/data/Summer19UL16_V7/Summer19UL16_RunFGH_V7_DATA_L2Relative_AK4PFchs.txt'
+DataL3JetPar             = 'FWLJMET/LJMet/data/Summer19UL16_V7/Summer19UL16_RunFGH_V7_DATA_L3Absolute_AK4PFchs.txt'
+DataResJetPar            = 'FWLJMET/LJMet/data/Summer19UL16_V7/Summer19UL16_RunFGH_V7_DATA_L2L3Residual_AK4PFchs.txt'
+DataL1JetParAK8          = 'FWLJMET/LJMet/data/Summer19UL16_V7/Summer19UL16_RunFGH_V7_DATA_L1FastJet_AK8PFPuppi.txt'
+DataL2JetParAK8          = 'FWLJMET/LJMet/data/Summer19UL16_V7/Summer19UL16_RunFGH_V7_DATA_L2Relative_AK8PFPuppi.txt'
+DataL3JetParAK8          = 'FWLJMET/LJMet/data/Summer19UL16_V7/Summer19UL16_RunFGH_V7_DATA_L3Absolute_AK8PFPuppi.txt'
+DataResJetParAK8         = 'FWLJMET/LJMet/data/Summer19UL16_V7/Summer19UL16_RunFGH_V7_DATA_L2L3Residual_AK8PFPuppi.txt'
 
 ## El MVA ID
 UseElIDV1_ = False #False means using ElIDV2
