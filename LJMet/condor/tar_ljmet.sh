15b7618fd380d978e21394e089265d4d652d093b
dsunyou <daniel_li2@brown.edu>
2022-02-03 16:49:34 -0600

    remove files that aren't used for UL

--- LJMet/condor/tar_ljmet.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-thisDir=$PWD
-
-cd $PWD/../../../ #make sure this is ${CMSSW_BASE}/src/
-
-tar --exclude "*/.git" --exclude "git*" --exclude "FWLJMET/LJMet/CRAB3" --exclude "FWLJMET/LJMet/condor" -zcvf $thisDir/ljmet.tar *
-
-cd -
