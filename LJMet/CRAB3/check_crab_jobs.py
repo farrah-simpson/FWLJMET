ac57542f94a7f8f569c12e0bf4cd9e6e443a2005
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2022-02-04 17:41:24 -0500

    Update and rename check_status_multiple_crab_jobs.py to check_crab_jobs.py

---
+++ LJMet/CRAB3/check_crab_jobs.py
@@ -0,0 +1,36 @@
+import os, argparse, imp
+
+parser = argparse.ArgumentParser()
+parser.add_argument( "-f", "--finalstate", action ="store", required = True )
+parser.add_argument( "-y", "--year", action ="store", required = True )
+parser.add_argument( "--resubmit", action ="store_true", default=False)
+parser.add_argument( "--report", action ="store_true", default=False)
+parser.add_argument( "--kill", action ="store_true", default=False)
+parser.add_argument( "--verbose", action = "store_true" )
+option = parser.parse_args()
+
+#Sample list file
+sampleListPath = "sample_list_{}20{}UL.py".format( option.finalstate, option.year )
+samples = imp.load_source( "Sample", sampleListPath, open(sampleListPath,"r") )
+
+def check_crab_jobs( sample_dict, crab_dir ):
+  for dataset in sample_dict:
+    if option.resubmit:
+      print( ">> Resubmitting {}: {}".format( dataset, sample_dict[dataset] )
+      os.system( "crab resubmit {}".format( os.path.join( crab_dir, "crab_UL{}_{}".format( option.year, sample ) )
+    elif option.kill:
+      print '\nAttempting to kill ',dataset,':',sample_dict[dataset]
+      os.system('crab kill '+crab_submit_dir)
+    else:
+      print( ">> Checking status {}: {}".format( dataset, sample_dict[dataset] )
+      os.system( "crab status ".format( os.path.join( crab_dir, "crab_UL{}_{}".format( option.year, sample ) )
+
+
+
+if __name__ == '__main__':
+
+  check_status_multiple_crab_jobs(sample.signaldict)
+  check_status_multiple_crab_jobs(sample.bkgdict)
+  check_status_multiple_crab_jobs(sample.ttbarbkgdict)
+  check_status_multiple_crab_jobs(sample.datadict)
+
