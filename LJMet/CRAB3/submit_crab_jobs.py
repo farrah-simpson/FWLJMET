bd2c9f3e1ae4f300406b2752a1c007d6e164520f
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2022-02-04 17:27:19 -0500

    Update and rename submit_mutiple_crab_jobs.py to submit_crab_jobs.py

---
+++ LJMet/CRAB3/submit_crab_jobs.py
@@ -0,0 +1,31 @@
+import os, argparse, imp
+
+parser = argparse.ArgumentParser()
+parser.add_argument( "-f", "--finalState", required = True, help = "singleLep,doubleLep,multiLep" )
+parser.add_argument( "-y", "--year", required = True, help = "16APV,16,17,18" )
+parser.add_argument( "--test", action = "store_true" )
+option = parser.parse_args()
+
+#Sample list file
+sampleListPath = "sample_list_{}20{}UL.py".format( option.finalState, option.year )
+sample = imp.load_source( "Sample", sampleListPath, open(sampleListPath,"r") )
+
+home = os.environ['HOME']
+CRABCONFIG_DIR = "crab_configs_{}20{}UL".format( option.finalState, option.year )
+
+def submit_crab_jobs( sample_dict ):
+  for dataset in sample_dict:
+    print( "Submitting {}: {}".format( dataset, sample_dict[ dataset ] ) )
+    crab_cfg = os.path.join( CRABCONFIG_DIR, "crab_config_{}.py".format( dataset ) )
+    
+    os.system( "crab submit {}".format( crab_cfg ) )
+    
+
+if __name__ == '__main__':
+  if option.test:
+    submit_crab_jobs(sample.testdict)
+  else:
+    submit_crab_jobs(sample.signaldict)
+    submit_crab_jobs(sample.bkgdict)
+    submit_crab_jobs(sample.ttbarbkgdict)
+    submit_crab_jobs(sample.datadict)
