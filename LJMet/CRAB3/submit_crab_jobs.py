c566f64fb0026cfd00e2632e2dfda4920bcb9cb4
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2022-02-04 22:14:38 -0500

    Update submit_crab_jobs.py

--- LJMet/CRAB3/submit_crab_jobs.py
+++ LJMet/CRAB3/submit_crab_jobs.py
@@ -3,6 +3,7 @@ import os, argparse, imp
 parser = argparse.ArgumentParser()
 parser.add_argument( "-f", "--finalState", required = True, help = "singleLep,doubleLep,multiLep" )
 parser.add_argument( "-y", "--year", required = True, help = "16APV,16,17,18" )
+parser.add_argument( "--shifts", action = "store_true" )
 parser.add_argument( "--test", action = "store_true" )
 option = parser.parse_args()
 
@@ -13,10 +14,13 @@ sample = imp.load_source( "Sample", sampleListPath, open(sampleListPath,"r") )
 home = os.environ['HOME']
 CRABCONFIG_DIR = "crab_configs_{}20{}UL".format( option.finalState, option.year )
 
-def submit_crab_jobs( sample_dict ):
+def submit_crab_jobs( sample_dict, shifts ):
   for dataset in sample_dict:
     print( "Submitting {}: {}".format( dataset, sample_dict[ dataset ] ) )
-    crab_cfg = os.path.join( CRABCONFIG_DIR, "crab_config_{}.py".format( dataset ) )
+    if shifts:
+      crab_cfg = os.path.join( CRABCONFIG_DIR, "crab_config_{}.py".format( dataset ) )
+    else:
+      crab_cfg = os.path.join( CRABCONFIG_DIR, "crab_config_shifts_{}.py".format( dataset ) )
     
     os.system( "crab submit {}".format( crab_cfg ) )
     
