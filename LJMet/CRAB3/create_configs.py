b7d781670e96dbe06a404757ca17555923fab40e
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2022-02-05 11:02:11 -0500

    Update create_configs.py

--- LJMet/CRAB3/create_configs.py
+++ LJMet/CRAB3/create_configs.py
@@ -8,13 +8,18 @@ relBase = os.environ['CMSSW_BASE']
 home = os.environ['HOME']
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--finalState",action="store")
-parser.add_argument("--year",action="store")
+parser.add_argument("-f","--finalState",action="store")
+parser.add_argument("-y","--year",action="store")
+parser.add_argument("--shifts",action="store_true")
 parser.add_argument("--nominalTreeOnly",action="store_true")
 parser.add_argument("--brux",action="store_true")
-parser.add_argument("--outfolder",action="store",default="FWLJMET_crab_output")
+parser.add_argument("-o","--outfolder",action="store",default="crab_log")
 option = parser.parse_args()
 
+if option.finalState not in [ "singleLep", "doubleLep", "multiLep" ]: quit( "[ERR] Invalid -f (--finalState) argument passed." )
+if option.year not in [ "2016APV", "2016", "2017", "2018" ]: quit( "[ERR] Invalid -y (--year) argument passed." )
+if option.shifts: SHIFTS = "True" else "False"
+	
 #Sample list file
 sampleListPath = "sample_list_"+option.finalState+option.year+"UL.py"
 sample = imp.load_source("Sample",sampleListPath,open(sampleListPath,"r"))
@@ -23,31 +28,33 @@ sample = imp.load_source("Sample",sampleListPath,open(sampleListPath,"r"))
 ### SET YOUR STRINGS
 ####################
 #cmsRun config
-runScript = option.finalState+option.year + "UL" if option.nominalTreeOnly else option.finalState+option.year+'_multipleTree_UL'
 
-CMSRUNCONFIG        = '../runFWLJMet_'+runScript+'.py'
+CMSRUNCONFIG        = '../runFWLJMet_{}{}UL.py'.format( option.finalState, option.year )
 
 #folder to save the created crab configs
-CRABCONFIG_DIR      = 'crabConfigs_'+option.finalState+option.year + "UL"
+CRABCONFIG_DIR      = 'crab_configs_{}{}UL'.format( option.finalState, option.year )
 
 #the crab cfg template to copy from
-CRABCONFIG_TEMPLATE = 'crab_FWLJMET_cfg_template.py'
+CRABCONFIG_TEMPLATE = 'crab_config_template.py'
 
 #crab request name
 REQNAME             = option.finalState+option.year + "UL"
 
 #eos out folder
-OUTFOLDER           = option.outfolder
+OUTFOLDER           = "{}_{}{}UL".format( option.outfolder, option.finalState, option.year ) if option.outfolder == "crab_log" else option.outfolder
 
 #JSON for Data
-JSONFORDATA         = "https://cms-service-dqmdc.web.cern.ch/CAF/certification/Collisions17/13TeV/Legacy_2017/Cert_294927-306462_13TeV_UL2017_Collisions17_GoldenJSON.txt"
-if option.year == '2018': 
-	JSONFORDATA = "https://cms-service-dqmdc.web.cern.ch/CAF/certification/Collisions18/13TeV/Legacy_2018/Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON.txt"
-elif option.year == '2016':
-	JSONFORDATA = "https://cms-service-dqmdc.web.cern.ch/CAF/certification/Collisions16/13TeV/Legacy_2016/Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON.txt" 
-elif option.year == "2016APV":
-	JSONFORDATA = "https://cms-service-dqmdc.web.cern.ch/CAF/certification/Collisions16/13TeV/Legacy_2016/Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON.txt"
-	
+JSONDATA = {
+  "16": "https://cms-service-dqmdc.web.cern.ch/CAF/certification/Collisions16/13TeV/Legacy_2016/Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON.txt", # UL URL
+  #"16": "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Legacy_2016/Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON.txt", # UL lxplus
+  "16APV": "https://cms-service-dqmdc.web.cern.ch/CAF/certification/Collisions16/13TeV/Legacy_2016/Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON.txt", # UL URL
+  #"16APV": "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Legacy_2016/Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON.txt", # UL lxplus
+  "17": "https://cms-service-dqmdc.web.cern.ch/CAF/certification/Collisions17/13TeV/Legacy_2017/Cert_294927-306462_13TeV_UL2017_Collisions17_GoldenJSON.txt", # UL URL
+  #"17": "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/Legacy_2017/Cert_294927-306462_13TeV_UL2017_Collisions17_GoldenJSON.txt", # UL lxplus
+  "18": "https://cms-service-dqmdc.web.cern.ch/CAF/certification/Collisions18/13TeV/Legacy_2018/Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON.txt", # UL URL
+  #"18": "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/13TeV/Legacy_2018/Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON.txt" # UL lxplus
+}
+
 if option.brux:
 	OUTPATH = '/store/group/bruxljm/'
 	STORESITE = 'T3_US_Brown'
@@ -61,8 +68,8 @@ def create_crab_config_files_from_template(sample_dict,**kwargs):
 
 		print dataset,sample_dict[dataset]
 
-		filename = 'crab_FWLJMET_cfg_'+dataset+'.py'
-		cmsRunname = 'run_FWLJMET_'+dataset+'.py'
+		filename = 'crab_config_'+dataset+'.py'
+		cmsRunname = 'runFWLJMet_'+dataset+'.py'
 
 		#copy template file to new directory
 		os.system('cp -v '+CRABCONFIG_TEMPLATE+' '+CRABCONFIG_DIR+'/'+filename)
@@ -90,6 +97,7 @@ def create_crab_config_files_from_template(sample_dict,**kwargs):
 		os.system("sed -i 's|ISVLQSIGNAL|"+kwargs['ISVLQSIGNAL']+"|g' "+CRABCONFIG_DIR+"/"+cmsRunname)		
 		os.system("sed -i 's|ISTTBAR|"+kwargs['ISTTBAR']+"|g' "+CRABCONFIG_DIR+"/"+cmsRunname)
 		os.system("sed -i 's|DOGENHT|"+kwargs['DOGENHT']+"|g' "+CRABCONFIG_DIR+"/"+cmsRunname)
+		os.system("sed -i 's|SHIFTS|" + kwargs["SHIFTS"] + "|g' " + CRABCONFIG_DIR + "/" + filename)
 
 		os.system("sed -i 's|OUTPATH|"+OUTPATH+"|g' "+CRABCONFIG_DIR+"/"+filename)
 		os.system("sed -i 's|STORESITE|"+STORESITE+"|g' "+CRABCONFIG_DIR+"/"+filename)
@@ -106,6 +114,7 @@ if __name__=='__main__':
 		ISVLQSIGNAL='False',
 		ISTTBAR='False',
  		DOGENHT='True',
+		SHIFTS=SHIFTS
 		)
 
 	#### Bkg MC - no ttbar - no MLM
@@ -115,6 +124,7 @@ if __name__=='__main__':
 		ISVLQSIGNAL='False',
 		ISTTBAR='False',
  		DOGENHT='False',
+		SHIFTS=SHIFTS
 		)
 
 	#### Bkg MC - ttbar
@@ -124,6 +134,7 @@ if __name__=='__main__':
  		ISVLQSIGNAL='False',
  		ISTTBAR='True',
  		DOGENHT='False',
+		SHIFTS=SHIFTS
  		)
 
         #### fourtops MC
@@ -132,21 +143,24 @@ if __name__=='__main__':
                 ISMC='True',
                 ISVLQSIGNAL='False',
                 ISTTBAR='False',
- 		  DOGENHT='False',
+ 		DOGENHT='False',
+		SHIFTS=SHIFTS
                 )
         create_crab_config_files_from_template(
                 sample.fourtopsttdict,
                 ISMC='True',
                 ISVLQSIGNAL='False',
                 ISTTBAR='True',
- 		  DOGENHT='False',
+ 		DOGENHT='False',
+		SHIFTS=SHIFTS
                 )
         create_crab_config_files_from_template(
                 sample.fourtopsbkgdict,
                 ISMC='True',
                 ISVLQSIGNAL='False',
                 ISTTBAR='False',
- 		  DOGENHT='False',
+ 		DOGENHT='False',
+		SHIFTS=SHIFTS
                 )
 
 	#### VLQ signal MC
@@ -156,6 +170,7 @@ if __name__=='__main__':
 		ISVLQSIGNAL='True',
 		ISTTBAR='False',
 		DOGENHT='False',
+		SHIFTS=SHIFTS
 		)
 
 	#### Data
@@ -165,4 +180,5 @@ if __name__=='__main__':
 	 	ISVLQSIGNAL='False',
 	 	ISTTBAR='False',
 	 	DOGENHT='False',
+		SHIFTS="False"
 	 	)
