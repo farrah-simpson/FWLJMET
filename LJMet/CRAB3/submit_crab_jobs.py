536436491b3fe1feb003448f3775add2fee3e2b7
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2022-02-08 17:47:23 -0500

    Update submit_crab_jobs.py

--- LJMet/CRAB3/submit_crab_jobs.py
+++ LJMet/CRAB3/submit_crab_jobs.py
@@ -2,34 +2,35 @@ import os, argparse, imp
 
 parser = argparse.ArgumentParser()
 parser.add_argument( "-f", "--folder", required = True, help = "singleLep,doubleLep,multiLep" )
+parser.add_argument( "-g", "--groups", nargs = "+", required = True )
 parser.add_argument( "--shifts", action = "store_true" )
-parser.add_argument( "--test", action = "store_true" )
 option = parser.parse_args()
 
 #Sample list file
 postfix = option.folder.split( "_" )[-1].strip("/") 
 sampleListPath = "sample_list_{}.py".format( postfix )
-sample = imp.load_source( "Sample", sampleListPath, open(sampleListPath,"r") )
+samples = imp.load_source( "Sample", sampleListPath, open(sampleListPath,"r") )
 
 home = os.environ['HOME']
 CRABCONFIG_DIR = option.folder
 
-def submit_crab_jobs( sample_dict, shifts ):
-  for dataset in sample_dict:
-    print( "Submitting {}: {}".format( dataset, sample_dict[ dataset ] ) )
-    if shifts:
-      crab_cfg = os.path.join( CRABCONFIG_DIR, "crab_config_shifts_{}.py".format( dataset ) )
-    else:
-      crab_cfg = os.path.join( CRABCONFIG_DIR, "crab_config_{}.py".format( dataset ) )
-    
-    os.system( "crab submit {}".format( crab_cfg ) )
-    
+def submit_crab_job( group, process, shifts ):
+  if shifts:
+    crab_cfg = os.path.join( CRABCONFIG_DIR, "crab_config_shifts_{}.py".format( process ) )
+  else:
+    crab_cfg = os.path.join( CRABCONFIG_DIR, "crab_config_{}.py".format( process ) )
+  os.system( "crab submit {}".format( crab_cfg ) )
 
 if __name__ == '__main__':
-  if option.test:
-    submit_crab_jobs(sample.testdict,option.shifts)
-  else:
-    submit_crab_jobs(sample.signaldict,option.shifts)
-    submit_crab_jobs(sample.bkgdict,option.shifts)
-    submit_crab_jobs(sample.ttbarbkgdict,option.shifts)
-    submit_crab_jobs(sample.datadict,option.shifts)
+  print( "[START] Submitting CRAB3 jobs" )
+  count = 0
+	for group in option.groups:
+    print( ">> Submitting jobs for {}".format( group ) )
+		if group not in list( samples.groups.keys() ):
+			print( "[WARN] {} is not a valid group listed in 'sample_list_{}{}UL.py'. Skipping.".format( group, option.finalState, option.year ) )
+		else:
+			for process in sample.groups[ group ]:
+        print( "   + {}: {}".format( process, samples.groups[ group ][ process ].split("/")[1] ) )
+				submit_crab_job( group, process, option.shifts )
+        count += 1
+  print( "[DONE] Submitted {} CRAB jobs.".format( count ) )
