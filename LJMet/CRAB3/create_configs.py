1e6ab3664ac176910d0fd9d1a86c373fffbe5865
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2022-02-09 12:45:55 -0500

    Update create_configs.py

--- LJMet/CRAB3/create_configs.py
+++ LJMet/CRAB3/create_configs.py
@@ -28,23 +28,6 @@ samples = imp.load_source( "Sample", sampleListPath, open( sampleListPath, "r" )
 ### SET YOUR STRINGS
 ####################
 
-CMSRUNCONFIG        = '../runFWLJMet_{}{}UL.py'.format( option.finalState, option.year )
-
-#folder to save the created crab configs
-CRABCONFIG_DIR      = 'crab_configs_{}{}UL'.format( option.finalState, option.year )
-
-#folder to store submit logs
-CRABSUBMIT_DIR      = "crab_submit_{}{}UL".format( option.finalState, option.year )
-
-#the crab cfg template to copy from
-CRABCONFIG_TEMPLATE = 'crab_config_template.py'
-
-#crab request name
-REQNAME             = option.finalState+option.year + "UL"
-
-#eos out folder
-OUTFOLDER           = "FWLJMET106XUL_{}{}UL_RunIISummer20".format( option.finalState, option.year ) if option.outfolder == "default" else option.outfolder
-
 #JSON for Data
 JSONDATA = {
   "2016": "https://cms-service-dqmdc.web.cern.ch/CAF/certification/Collisions16/13TeV/Legacy_2016/Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON.txt", # UL URL
@@ -65,6 +48,23 @@ else:
 	STORESITE = 'T3_US_FNALLPC'
 
 def create_crab_config( group, process, shifts ):
+	CMSRUNCONFIG        = '../runFWLJMet_{}{}UL.py'.format( option.finalState, option.year )
+
+	#folder to save the created crab configs
+	CRABCONFIG_DIR      = 'crab_configs_{}{}UL'.format( option.finalState, option.year )
+
+	#folder to store submit logs
+	CRABSUBMIT_DIR      = "crab_submit_{}{}UL".format( option.finalState, option.year )
+
+	#the crab cfg template to copy from
+	CRABCONFIG_TEMPLATE = 'crab_config_template.py'
+
+	#crab request name
+	REQNAME             = option.finalState+option.year + "UL"
+
+	#eos out folder
+	OUTFOLDER           = "FWLJMET106XUL_{}{}UL_RunIISummer20".format( option.finalState, option.year ) if option.outfolder == "default" else option.outfolder
+	
 	ISMC = "True" if process not in samples.groups[ "DATA" ].keys() else "False"
 	SHIFTS = "True" if shifts else "False"
 	try: 
@@ -79,6 +79,8 @@ def create_crab_config( group, process, shifts ):
 		DOGENHT = "True" if ( ( process in samples.groups[ "WJETSHT" ].keys() ) or ( process in samples.groups[ "QCDHT" ].keys() ) or ( process in samples.groups[ "DYMHT" ].keys() ) ) else "False"
 	except:
 		DOGENHT = "False"
+	if group == "TEST":
+		OUTFOLDER += "_test"
 		
 	filename = 'crab_config_shifts_{}.py'.format( process ) if SHIFTS else "crab_config_{}.py".format( process )
 	cmsRunname = 'runFWLJMet_shifts_{}.py'.format( process ) if SHIFTS else "runFWLJMet_{}.py".format( process )
